e2c0031a566bcea15237fe37164713d28d4f0fca
robertocajueiro <beto.cajueiro@gmail.com>
2022-10-02 23:35:07 -0300

    add get e set em personagem

--- aula03-visibilidade/Personagem.ts
+++ aula03-visibilidade/Personagem.ts
@@ -2,10 +2,10 @@ export default class Personagem{
 
     constructor( 
         private _nome: string, 
-        private energia: number, 
-        private vida: number,
-        private ataque: number,
-        private defesa: number
+        private _energia: number, 
+        private _vida: number,
+        private _ataque: number,
+        private _defesa: number
         ){}
 
         // Toda vez que pensar em ler dados na classe - use parametros
@@ -15,52 +15,96 @@ export default class Personagem{
         public get nome() : string {
             return this._nome;
         }
+
         
+        public set nome(nome : string) {
+            this._nome = nome;
+        }
+
+        
+        public get energia() : number {
+            return this._energia;
+        }
+        
+        
+        public set energia(energia : number) {
+            this._energia = energia;
+        }
+
+        
+        public get vida() : number {
+            return this._vida;
+        }
+
+        
+        public set vida(vida : number) {
+            this._vida = vida;
+        }
+        
+        
+        public get ataque() : number {
+            return this._ataque
+        } 
+
+        
+        public set ataque(ataque : number) {
+            this._ataque = ataque;
+        }
+        
+        
+        public get defesa() : number {
+            return this._defesa;
+        }
+        
+        
+        public set defesa(defesa : number) {
+            this._defesa = defesa;
+        }
 
     public status(): string{
         return (
             "Guerreiro: \n" + 
             "\nNome: " + 
             this.nome +
-            ("\nEnergia: " + this.energia.toFixed(1)) +
-            ("\nAtaque: " + this.ataque.toFixed(1)) +
-            ("\nDefesa: " + this.defesa.toFixed(1))
+            ("\nEnergia: " + this._energia.toFixed(1)) +
+            ("\nAtaque: " + this._ataque.toFixed(1)) +
+            ("\nDefesa: " + this._defesa.toFixed(1))
         );
         
        
     }
 
     public treinarAtacar(): void{
-        this.ataque += this.randomizar(7);
-        this.energia -= this.randomizar(10);
-            if(this.defesa> 100){
-                this.defesa = 0;
+        this._ataque += this.randomizar(7);
+        this._energia -= this.randomizar(10);
+            if(this._defesa> 100){
+                this._defesa = 0;
             }
     }
 
     public treinarDefesa(): void {
-        this.defesa += this.randomizar(5);
-        this.energia += this.randomizar(10);
-         if(this.defesa > 100){
-            this.defesa = 0;
+        this._defesa += this.randomizar(5);
+        this._energia += this.randomizar(10);
+         if(this._defesa > 100){
+            this._defesa = 0;
             }       
     }
 
     public descansar(hour: number): void {
-        this.energia += hour * this.randomizar(10);
-        if(this.energia > 100){
-            this.energia * 100;
+        this._energia += hour * this.randomizar(10);
+        if(this._energia > 100){
+            this._energia * 100;
         }
     }
 
     public batalhar(): number {
         let desgaste : number = this.randomizar(10);
-        this.energia -= desgaste;
+        this._energia -= desgaste;
         return desgaste;
     }
 
     public isDead(): boolean {
-       return this.energia < 0;
+       return this._energia < 0;
     
     }
 
